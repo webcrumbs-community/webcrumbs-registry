ce68a0e5f849d07a6f495da869e19fa8ced5e4ad
proudlyadvance <julia@juliamachado.com.br>
2023-09-20 08:45:37 -0300

    Updated github workflows

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,7 +3,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+// const domain = process.env.PRODUCTION_DOMAIN;
+const domain = 'https://dkpgqiynfz7yt.cloudfront.net/';
 
 const prodConfig = {
   mode: 'production',
