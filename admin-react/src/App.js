a31600bf08a1a2c242e2d4dc307f45b7fe481eaa
proudlyadvance <julia@juliamachado.com.br>
2023-10-23 17:02:29 -0300

    Connected Marketing to Admin-Next

--- admin-react/src/App.js
+++ admin-react/src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { mount } from 'marketing/Marketing';
+import Marketing from "./components/Marketing";
 
 const App = () => {
-  console.log(mount);
   return (
     <div>
       something
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">Landing</Route>
-          <Route path="/price">Pricing</Route>
-        </Routes>
-      </BrowserRouter>
+      <Marketing />
     </div>
   );
 };
---
+++ admin-react/src/components/Marketing.js
@@ -0,0 +1,13 @@
+// TO-DO: fetch plugin list from db and lazy load
+import { mount } from 'marketing/Marketing';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        mount(ref.current);
+    }, []);
+
+    return <div ref={ref} />;
+};
