ac550d7094d37abc3a73676bcb67c88816bb6074
proudlyadvance <julia@juliamachado.com.br>
2023-10-23 13:50:30 -0300

    Bundled Admin

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       remotes,
       filename: "remoteEntry.js",
       exposes: {
-        "./Admin": "./src/bootstrap.js",
+        "./Admin": "./src/admin.js",
       },
     }),
     new HtmlWebpackPlugin({
