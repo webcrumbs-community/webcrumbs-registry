24e9de19ca2a29d31c6702171e0eb21c302be81d
proudlyadvance <julia@juliamachado.com.br>
2023-10-23 17:29:40 -0300

    Added production webpack config

---
+++ admin-react/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/admin/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "admin",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
--- plugins/marketing-react/config/webpack.dev.js
+++ plugins/marketing-react/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -18,9 +17,6 @@ const devConfig = {
         "./Marketing": "./src/bootstrap.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
