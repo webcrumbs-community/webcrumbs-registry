9fa7f659032c8a7f6824a7724b0b12df246457e0
proudlyadvance <julia@juliamachado.com.br>
2023-10-24 13:15:03 -0300

    CI/CD

--- admin-react/src/bootstrap.js
+++ admin-react/src/bootstrap.js
@@ -2,17 +2,5 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const mount = (el) => {
-  console.log("mounting");
-  const root = createRoot(el);
-  root.render(<App />);
-};
-
-const el = document.querySelector("#dev-admin");
-console.log(el);
-
-if (el) {
-  mount(el);
-}
-
-export { mount };
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
